1fdfaabee3294a8520423ea20b8bdcbac7cde447
rjcarney <33989142+rjcarney@users.noreply.github.com>
2021-09-22 12:26:26 -0400

    Video Complete
    Follow through with video complete

--- my-app/App.js
+++ my-app/App.js
@@ -2,6 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Constants } from 'expo-constants';
+import firebase from 'firebase/app';
+
+if(firebase.apps.length == 0){
+  firebase.initializeApp(Constants.manifest.web.config.firebase)
+}
 export default function App() {
   return (
     <View style={styles.container}>
